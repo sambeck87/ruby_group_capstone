f1c29eebd1b36af043534452da77fd8dbda0d700
sambeck87 <sandro_mipb@hotmail.com>
2023-03-20 19:39:03 -0600

    Add Game class Add can_be_archived? method Add Author class Add add_item method Add list_author method in app Add add_game method in app Add preserve.rb file Add data folder

--- classes/item.rb
+++ classes/item.rb
@@ -2,12 +2,11 @@ require 'securerandom'
 require 'date'
 
 class Item
-  attr_accessor :genre, :source, :author, :label, :publish_date
+  attr_accessor :genre, :author, :label, :publish_date
 
-  def initialize(genre, author, source, label, publish_date)
+  def initialize(genre, author, label, publish_date)
     @genre = genre
     @author = author
-    @source = source
     @label = label
     @publish_date = Date.parse(publish_date)
     @id = SecureRandom.uuid
@@ -24,6 +23,3 @@ class Item
     (Date.today - @publish_date).to_f / 365 > 10
   end
 end
-
-date = Item.new(nil, nil, nil, nil, '2010-10-09')
-p date.can_be_archived?
