cc1c35bc951f3a58e18db40114079749d7eec193
Juank628 <jsanchez_zunino@yahoo.com>
2023-03-21 12:48:33 -0500

    Created attr_accessors for Book class

--- classes/book.rb
+++ classes/book.rb
@@ -1,6 +1,8 @@
 require_relative './item'
 
 class Book < Item
+  attr_accessor :publisher, :cover_state
+
   def initialize(genre, author, label, publish_date, publisher)
     super(genre, author, label, publish_date)
     @publisher = publisher
