6239384f40e99cb2f2bedb33334ccb8b83c6cddc
sambeck87 <sandro_mipb@hotmail.com>
2023-03-21 18:09:44 -0600

    Create squema.sql Create item table Create author table Create game table

---
+++ data_base/squema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE item (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  source_id INT,
+  genre_id INT,
+  author_id INT,
+  label_id INT,
+  publish_date DATE,
+  archived BOOLEAN,
+  FOREIGN KEY (genre_id) REFERENCES genres (id),
+  FOREIGN KEY (author_id) REFERENCES authors (id),
+  FOREIGN KEY (label_id) REFERENCES labels (id),
+  FOREIGN KEY (source_id) REFERENCES sources (id)
+)
+
+CREATE TABLE author (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50)
+)
+
+CREATE TABLE game(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  multiplayer BOOLEAN,
+  last_played_at DATE,
+  publish_date DATE
+  item_id INT,
+  FOREIGN KEY(item_id) REFERENCES item(id) ON DELETE CASCADE
+);
+
