ac9e31cf3c92fb9b8af61ce66037bfd4f4453821
Juank628 <jsanchez_zunino@yahoo.com>
2023-03-22 10:50:48 -0500

    Added create_label and by_id methods in Label class

--- classes/label.rb
+++ classes/label.rb
@@ -12,6 +12,13 @@ class Label
     ObjectSpace.each_object(self).to_a
   end
 
+  def self.create_label(title, color)
+    title = title.capitalize
+    color = color.downcase
+    label = all.find { |lab| lab.title == title }
+    label.nil? ? new(title, color) : label
+  end
+
   def self.by_id(id)
     all.find { |label| label.id == id }
   end
