efb0e52df25a5eca70ba41bd1c52e1f6c50cf047
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-03-21 20:35:17 -0500

    Add to_json and static all mthods.

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,5 +1,5 @@
 class Genre
-  attr_accessor :name
+  attr_accessor :name, :id
 
   def initialize(name, id = SecureRandom.uuid)
     @name = name
@@ -10,5 +10,16 @@ class Genre
   def add_item(item)
     @items << item
     item.genre = self
+  end
+
+  def to_json(*_option)
+    {
+      id: @id,
+      name: @name
+    }.to_json
+  end
+
+  def self.all
+    ObjectSpace.each_object(self).to_a
   end
 end
