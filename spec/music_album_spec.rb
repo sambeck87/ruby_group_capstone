01a4616c8bafdd97aaed5907b818b81a1d29dbbc
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-03-22 17:43:10 -0500

    Add test cases for MusicAlbum and Genre methods.

---
+++ spec/music_album_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../classes/music_album'
+require_relative '../classes/genre'
+require_relative '../classes/label'
+require_relative '../classes/author'
+
+describe MusicAlbum do
+  context 'When testing the can_be_archived? method' do
+    it 'should return true if parent\'s method returns true and if on_spotify equals true.' do
+      genre = Genre.new('Rock')
+      author = Author.new('Paul', 'McCartney')
+      label = Label.new('Sony', 'Green')
+      album = MusicAlbum.new(genre, author, label, '2008-05-08', on_spotify: true)
+
+      expect(album.can_be_archived?).to eq true
+    end
+
+  end
+end
